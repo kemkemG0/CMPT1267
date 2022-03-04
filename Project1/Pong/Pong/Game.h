e866578acccc2f30625c743d99b30ed02ecf48ac
kemkemG0 <kenshin.jpsdeveloper@gmail.com>
2022-03-03 17:18:00 -0800

    update

---
+++ Project1/Pong/Pong/Game.h
@@ -0,0 +1,34 @@
+#pragma once
+// #include "SDL/SDL.h"
+#include <SDL2/SDL.h>
+
+struct Vector2
+{
+	float x;
+	float y;
+};
+
+class Game
+{
+public:
+	Game();
+	bool Initialize();
+	void RunLoop();
+	void Shutdown();
+private:
+	void ProcessInput();
+	void UpdateGame();
+	void GenerateOutput();
+
+	SDL_Window* mWindow;
+	SDL_Renderer* mRenderer;
+	Uint32 mTicksCount;
+	
+	short int bg_color[3]={0, 0, 255};
+	
+	bool mIsRunning;
+	int mPaddleDir;
+	Vector2 mPaddlePos;
+	Vector2 mBallPos;
+	Vector2 mBallVel;
+};
